909e593ae9f5fe338ab19b4ed9d52bb0ea09bad8
Jade Lovelace <lix@jade.fyi>
2024-06-18 23:32:31 -0700

    Return Lix module to main, with fixes from release-2.90 work

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.lix = {
-    url = "https://git.lix.systems/lix-project/lix/archive/release-2.90.tar.gz";
+    url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
     flake = false;
   };
   inputs.flake-utils.url = "github:numtide/flake-utils";
